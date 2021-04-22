6de2b456d6c97d324a8b41c429f8246dd4562495
TroySandy <troysandy16@gmail.com>
2021-04-22 15:02:59 -0400

    working endpoints for review controller

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -6,38 +6,30 @@ router.get("/practice", (req, res) => {
   res.send("Hey!! This is a practice route!!")
 })
 
-/*
-=======================
-  Get All Reviews
-=======================
-*/
-
 router.get("/", async (req, res) => {
+  const {owner_id} = req.user;
   try {
     const entries = await Review.findAll({
       where : {
         owner_id: owner_id
-      }
+      } 
     });
-    res.send(200).json(entries);
+    res.status(200).json({
+      entries,
+      message: "here is your movie" 
+    });
+    
   } catch (err) {
     res.status(500).json({ error: err})
   }
 }); 
 
-/*
-=======================
-  Get One Review
-=======================
-*/
-router.get("/onemovie/:id", async (req, res) => {
-
-
+router.get("/", async (req, res) => {
   try {
     const entries = await Review.findAll({
       where: {movie_id: movie_id}
     });
-    res.send(200).json(entries);
+    res.status(200).json(entries);
   } catch (err) {
     res.status(500).json({ error: err })
   }
